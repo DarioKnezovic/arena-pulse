fa455a22938f1ef6ae6da1c974181af892060738
DarioKnezovic <dknezovic997@gmail.com>
2024-03-07 18:40:43 +0000

    Finished leagues page.

--- src/views/admin/Leagues.vue
+++ src/views/admin/Leagues.vue
@@ -1,8 +1,47 @@
 <template>
-  <div>hehe</div>
+  <h1>All leagues</h1>
+
+  <v-row class="mt-3">
+    <v-col cols="6">
+      <v-select
+        label="Sports"
+        v-model="sportFilter"
+        :items="sportStore.sports?.map(sport => sport.strSport)"
+      ></v-select>
+    </v-col>
+  </v-row>
+  <v-data-table
+      :headers="tableHeaders"
+      :items="filteredLeagues"
+      item-key="idLeague"
+      class="elevation-1"
+  />
 </template>
 
 <script setup lang="ts">
+  import { computed, onMounted, ref } from "vue";
+  import { useLeagueStore } from "../../stores/league";
+  import { useSportStore } from "../../stores/sport";
+
+  const leagueStore = useLeagueStore();
+  const sportStore = useSportStore();
+
+  const sportFilter = ref(null);
+  const tableHeaders = [
+    { title: 'League ID', key: 'idLeague' },
+    { title: 'League Name', key: 'strLeague' },
+    { title: 'Sport', key: 'strSport' },
+    { title: 'Alternate Names', key: 'strLeagueAlternate' },
+  ];
+
+  const filteredLeagues = computed(() => {
+    return sportFilter.value ? leagueStore.leagues.filter(league => league.strSport === sportFilter.value) : leagueStore.leagues;
+  });
+
+  onMounted(async () => {
+    await leagueStore.init();
+    await sportStore.init();
+  });
 </script>
 
 <style scoped>
